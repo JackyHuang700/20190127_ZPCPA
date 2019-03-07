21cc17511cae82da5d41eaf03b958c04c725c7dd
Jacky <jacky8595125@gmail.com>
2019-03-07 12:38:50 +0800

    ...

---
+++ wwwroot/webpackTest/index.bundle.01913b31.js
@@ -0,0 +1,160 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _scss_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_main_scss__WEBPACK_IMPORTED_MODULE_0__);
+// import printMe from './print'
+ // import '../css/styles.css'
+// import {
+//   cube,
+//   square,
+// } from './multiFunc'
+// // 在dll內的套件
+// var _ = require('lodash');
+// console.log('index');
+// console.log('lodashhhhh', _.defaults({ 'a': 1 }, { 'a': 3, 'b': 91 }))
+// function component() {
+//   var element = document.createElement('div');
+//   var btn = document.createElement('button');
+//   element.innerHTML = "Hello webpack"
+//   btn.innerHTML = 'Click me and check the console!adsasdf';
+//   btn.onclick = printMe;
+//   element.appendChild(btn);
+//   return element;
+// }
+// document.body.appendChild(component())
+// console.log(cube(5))
+// console.log(square(5))
+// // console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`)
+// if (module.hot) {
+//   // module.hot.accept('./print.js', function() {
+//   //   console.log('Accepting the updated printMe module in index.js!');
+//   //   printMe();
+//   // })
+// }
+// // 需要加上才會啟動HMR
+// if (module.hot) {
+//   module.hot.accept()
+// }
+// function another_adder (...numbers) {
+//   // Create a list of numbers in the function
+//   const other_nums = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+//   // here, we are using the spread operator to concatenate numbers and other_nums into the same array,
+//   // in the same line where it is initialized.
+//   const all_nums = [...numbers, ...other_nums]
+//   let total = 0
+//   all_nums.forEach((n, i) => {
+//       // Type check to avoid turning this thing into a string, or creating some other error.
+//       if (typeof n === 'number') {
+//           total += n
+//       } else {
+//           console.log('can\'t add item at index' + i + '.')
+//       }
+//   })
+//   return total
+// }
+// console.log(another_adder(1, 2, 3, 4, 5, 6, 7, 8))
+// let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+// console.log(x); // 1
+// console.log(y); // 2
+// console.log(z); // { a: 3, b: 4 }
+// [5, 6].map(n => console.log(n));
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=index.bundle.01913b31.js.map
